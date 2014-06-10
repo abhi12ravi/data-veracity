e4c0573988956e4c2de670b5477d167dc8d88539
samarthbhargav <samarth.bhargav92@gmail.com>
2014-06-10 18:00:56 +0530

    added api.py

---
+++ facebook_api/api.py
@@ -0,0 +1,19 @@
+
+
+
+def FacebookApi(object):
+
+
+    def __init__(self):
+        pass
+
+    def get_last_n_status(self, id, date, n):
+        """
+        id -> facebook id
+        date -> date from which to fetch statuses. so if 6th may is the date, fetch last n statuses before this date
+        n -> number of statues to return
+        """
+        pass
+
+
+
