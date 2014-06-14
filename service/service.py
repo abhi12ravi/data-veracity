a7d8d84be863ce02df367806e07e704c8e7dcf03
samarthbhargav <samarth.bhargav92@gmail.com>
2014-06-14 22:18:13 +0530

    added service and init

--- service/service.py
+++ service/service.py
@@ -1,5 +1,5 @@
 
-
+from ..data_veracity import data_veracity
 
 class UserService(object):
 
@@ -7,8 +7,11 @@ class UserService(object):
         pass
 
 
+    def add_new_user(self,user):
+        if not isinstance(user, entity.User):
+            raise TypeError("Must be of type user")
 
 
-
-
-
+if __name__ == "__main__":
+    service = UserService()
+    service.add_new_user(list())
