a775df07b972eeddae3d427e1a1127821e5db6cb
samarthbhargav <samarth.bhargav92@gmail.com>
2014-06-16 11:14:58 +0530

    changes to service and dao

--- service/service.py
+++ service/service.py
@@ -1,17 +1,32 @@
+import sys
+sys.path.append("..")
 
-from ..data_veracity import data_veracity
 
+from dao import dao
+from entity import entity
 class UserService(object):
 
     def __init__(self):
+        self._dao  = dao.UserDao()
+
+
+    def create_user(self,user):
+        if not isinstance(user, entity.User):
+            raise TypeError("Must be of type user")
+        pass
+
+    def delete_user(self, user_id):
         pass
 
 
-    def add_new_user(self,user):
+    def update_user(self, user_id, user):
         if not isinstance(user, entity.User):
             raise TypeError("Must be of type user")
+        pass
+
+    def get_all_users(self):
+        pass
 
 
 if __name__ == "__main__":
     service = UserService()
-    service.add_new_user(list())
